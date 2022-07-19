4da8e8268da19954ee9e462f832bfce59a2fdee0
Dongmin_Kim <66549954+dongmin30@users.noreply.github.com>
2022-07-19 22:16:55 +0900

    [dmkim] 기초 패키지 구성

---
+++ Apllication/src/main/java/boot/server/app/main/entity/ViewEntity.java
@@ -0,0 +1,15 @@
+package boot.server.app.main.entity;
+
+/**
+ * packageName    : boot.server.app.main.entity
+ * fileName       : ViewEntity
+ * author         : dongm
+ * date           : 2022-07-19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-19        dongm       최초 생성
+ */
+public class ViewEntity {
+}
