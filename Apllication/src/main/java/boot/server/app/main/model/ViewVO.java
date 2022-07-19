36342c55eca095a88539e55db634f8ad801eb3e6
Dongmin_Kim <66549954+dongmin30@users.noreply.github.com>
2022-07-19 22:17:32 +0900

    Revert "Revert "[dmkim] 기초 패키지 구성""
    This reverts commit 9d0a0574f99fad7e67db82ee8399979c1506e381.

---
+++ Apllication/src/main/java/boot/server/app/main/model/ViewVO.java
@@ -0,0 +1,15 @@
+package boot.server.app.main.model;
+
+/**
+ * packageName    : boot.server.app.main.model
+ * fileName       : ViewVO
+ * author         : dongm
+ * date           : 2022-07-19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-19        dongm       최초 생성
+ */
+public class ViewVO {
+}
